2fd3ff5efd0191eb81111a955ac3ce30605ab64a
Krzysztof Kubicki <krzysztof.kubicki@fancyfon.com>
2017-08-21 14:00:24 +0200

    Added dummy gameplay screen

---
+++ core/src/com/kree/keehoo/screens/GameplayScreen.java
@@ -0,0 +1,18 @@
+package com.kree.keehoo.screens;
+
+import com.kree.keehoo.MyGdxGame;
+
+public class GameplayScreen extends AbstractScreen {
+
+
+
+    public GameplayScreen(MyGdxGame game) {
+        super(game);
+        init();
+    }
+
+    private void init() {}
+
+
+
+}
