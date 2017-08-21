137bdc76179c04d09556c8ab9eb6ce865a69c5d4
Krzysztof Kubicki <krzysztof.kubicki@fancyfon.com>
2017-08-21 14:13:00 +0200

    GamePlayescreen adds player

--- core/src/com/kree/keehoo/screens/GameplayScreen.java
+++ core/src/com/kree/keehoo/screens/GameplayScreen.java
@@ -1,18 +1,41 @@
 package com.kree.keehoo.screens;
 
 import com.kree.keehoo.MyGdxGame;
+import com.kree.keehoo.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
 
-
+private Player player;
 
     public GameplayScreen(MyGdxGame game) {
         super(game);
         init();
     }
 
-    private void init() {}
+    private void init() {
+        initPlayer();
+    }
 
+    private void initPlayer() {
+        player = new Player();
+        stage.addActor(player);
+    }
 
+    @Override
+    public void render(float delta) {
+        super.render(delta);
 
+        update();
+
+        spriteBatch.begin();
+
+        stage.draw();
+
+        spriteBatch.end();
+    }
+
+    private void update() {
+        stage.act();
+
+    }
 }
