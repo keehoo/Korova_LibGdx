b0ae926c89bbb8e8c3e4b07af903bfc0063e25fe
Krzysztof Kubicki <krzysztof.kubicki@fancyfon.com>
2017-08-21 14:04:10 +0200

    Time between splash and gameplay screen

--- core/src/com/kree/keehoo/screens/SplashScreen.java
+++ core/src/com/kree/keehoo/screens/SplashScreen.java
@@ -1,15 +1,26 @@
 package com.kree.keehoo.screens;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.Timer;
+import com.badlogic.gdx.utils.Timer.Task;
 import com.kree.keehoo.MyGdxGame;
 
 public class SplashScreen extends AbstractScreen {
 
+    private static final int DELAY_SECONDS = 1;
     private Texture splashImage;
 
-    public SplashScreen(MyGdxGame game) {
+    public SplashScreen(final MyGdxGame game) {
         super(game);
         init();
+
+        Timer.schedule(new Task() {
+            @Override
+            public void run() {
+                System.out.println("Po 1 sekundzie pojawia sie game play screen");
+                game.setScreen(new GameplayScreen(game));
+            }
+        }, DELAY_SECONDS);
     }
 
     private void init() {
@@ -23,7 +34,7 @@ public class SplashScreen extends AbstractScreen {
 
         spriteBatch.begin();
 
-        spriteBatch.draw(splashImage, 0, 0 );
+        spriteBatch.draw(splashImage, 0, 0);
 
         spriteBatch.end();
     }
