e16b06a3f0f355ad3f52fc6607358859c4a1d5c2
Krzysztof <krzysztof@MacBook-Pro-Krzysztof.local>
2017-08-25 22:46:54 +0200

    Change the view port from stretch to fit

--- core/src/com/kree/keehoo/screens/AbstractScreen.java
+++ core/src/com/kree/keehoo/screens/AbstractScreen.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.kree.keehoo.MyGdxGame;
 
@@ -22,7 +23,7 @@ public abstract class AbstractScreen implements Screen {
     public AbstractScreen(MyGdxGame game) {
         this.game = game;
         createCamera();
-        stage = new Stage(new StretchViewport(MyGdxGame.WIDTH, MyGdxGame.HEIGHT, camera));
+        stage = new Stage(new FitViewport(MyGdxGame.WIDTH, MyGdxGame.HEIGHT, camera));
         spriteBatch = new SpriteBatch();
         Gdx.input.setInputProcessor(stage);
 
