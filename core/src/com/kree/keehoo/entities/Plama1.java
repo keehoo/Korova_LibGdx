4fd3f2a3e6686dae7debd4581a78fdd5579bd18e
Krzysztof <krzysztof@MacBook-Pro-Krzysztof.local>
2017-08-26 20:32:05 +0200

    Plama test added - works with the background

---
+++ core/src/com/kree/keehoo/entities/Plama1.java
@@ -0,0 +1,14 @@
+package com.kree.keehoo.entities;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+
+public class Plama1 extends Image {
+
+    public static final String U_65_NORMAL = "u65_normal.png";
+
+    public Plama1() {
+        super(new Texture(U_65_NORMAL));
+
+    }
+}
--- core/src/com/kree/keehoo/screens/GameplayScreen.java
+++ core/src/com/kree/keehoo/screens/GameplayScreen.java
@@ -1,12 +1,18 @@
 package com.kree.keehoo.screens;
 
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.kree.keehoo.MyGdxGame;
+import com.kree.keehoo.entities.Plama1;
 import com.kree.keehoo.entities.Player;
 
 public class GameplayScreen extends AbstractScreen {
+
+
+    private Image background;
 
     private Player player;
     private Button playerButton;
@@ -19,11 +25,21 @@ public class GameplayScreen extends AbstractScreen {
 
     @Override
     protected void init() {
+        System.out.println("Gameplayscreen init method");
+        initBackgroundImage();
+
         initPlayer();
-        initPlayerButton();
+       initPlayerButton();
+
+    }
+
+    private void initBackgroundImage() {
+        background = new Image(new Texture("u85_normal.png"));
+        stage.addActor(background);
     }
 
     private void initPlayerButton() {
+        System.out.println("Init player");
         playerButton = new Button(new Button.ButtonStyle());
         playerButton.setWidth(460);
         playerButton.setHeight(360);
@@ -38,11 +54,20 @@ public class GameplayScreen extends AbstractScreen {
             public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
 
                 player.reactOnClick();
+                showPlama1();
 
                 System.out.println("CLICK");
                 return super.touchDown(event, x, y, pointer, button);
             }
         });
+    }
+
+    private void showPlama1() {
+        Plama1 plama1 = new Plama1();
+        plama1.setX(MyGdxGame.WIDTH * 0.1f);
+        plama1.setY(MyGdxGame.HEIGHT* 0.6f);
+        stage.addActor(plama1);
+        plama1.setZIndex(0);
 
     }
 
@@ -53,6 +78,7 @@ public class GameplayScreen extends AbstractScreen {
 
     @Override
     public void render(float delta) {
+        System.out.println("GamePlayScreen render()");
         super.render(delta);
 
         update();
@@ -65,6 +91,7 @@ public class GameplayScreen extends AbstractScreen {
     }
 
     private void update() {
+        System.out.println("Gameplayscreen update()");
         stage.act();
 
     }
