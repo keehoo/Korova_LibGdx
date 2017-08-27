bf09e8111f4c347947af9bef1b25acee277bdbf3
Krzysztof <krzysztof@MacBook-Pro-Krzysztof.local>
2017-08-27 16:36:16 +0200

    Added result lists to Constants

--- core/src/com/kree/keehoo/entities/Results/Constants.java
+++ core/src/com/kree/keehoo/entities/Results/Constants.java
@@ -1,5 +1,9 @@
 package com.kree.keehoo.entities.Results;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Constants {
 
     public static final String STOMACH_NOT_FULL_YET = "u2_normal.png";
@@ -7,4 +11,22 @@ public class Constants {
     public static final int HORIZONTAL_PADDING = 15;
     public static final boolean DEBUG = true;
 
+
+
+    public static final List<String> listOfString = new ArrayList<String>(Arrays.asList("daisy", "daisy", "clover", "cloud"));
+    public static final List<String> skinny_milk = new ArrayList<String>(Arrays.asList( "daisy", "daisy", "clover", "cloud" ));
+    public static final List<String> fat_milk = new ArrayList<String>(Arrays.asList( "daisy", "clover", "clover", "cloud"));
+    public static final List<String> cream = new ArrayList<String>(Arrays.asList( "daisy", "clover", "cloud", "cloud"));
+    public static final List<String> getElectric_milk = new ArrayList<String>(Arrays.asList( "daisy", "clover", "cloud", "battery"));
+    public static final List<String> hot_milk = new ArrayList<String>(Arrays.asList( "daisy", "clover", "cloud", "pepper"));
+    public static final List<String> cocoa = new ArrayList<String>(Arrays.asList( "daisy", "clover", "choco", "pepper"));
+    public static final List<String> getHot_chocolate = new ArrayList<String>(Arrays.asList("choco", "choco", "choco", "pepper"));
+    public static final List<String> aztec_chocolate = new ArrayList<String>(Arrays.asList( "choco", "choco", "pepper", "pepper"));
+    public static final List<String> milk_shake = new ArrayList<String>(Arrays.asList( "daisy", "cloud", "clover", "snow"));
+    public static final List<String> choco_milk_shake = new ArrayList<String>(Arrays.asList("daisy", "cloud", "choco", "snow"));
+    public static final List<String> strawberry_milkshake  = new ArrayList<String>(Arrays.asList( "daisy", "cloud", "strawberry", "snow"));
+    public static final List<String> grog = new ArrayList<String>(Arrays.asList( "snow", "atom", "battery", "pepper" ));
+    public static final List<String> nuka_cola = new ArrayList<String>(Arrays.asList( "daisy", "atom", "snow", "battery" ));
+    public static final List<String> on_the_beach = new ArrayList<String>(Arrays.asList("atom", "atom", "atom", "atom" ));
+    public static final List<String> molloko_velocet = new ArrayList<String>(Arrays.asList("daisy", "cloud", "battery", "atom"));
 }
