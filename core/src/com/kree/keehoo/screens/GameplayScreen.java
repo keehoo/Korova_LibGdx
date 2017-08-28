1451d56c61e70c4da114376de8b02e87a529d9f3
Krzysztof Kubicki <krzysztof.kubicki@fancyfon.com>
2017-08-28 15:04:03 +0200

    Drag and drop

--- core/src/com/kree/keehoo/screens/GameplayScreen.java
+++ core/src/com/kree/keehoo/screens/GameplayScreen.java
@@ -1,8 +1,16 @@
 package com.kree.keehoo.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
 import com.kree.keehoo.MyGdxGame;
 import com.kree.keehoo.entities.Player;
 
@@ -19,36 +27,75 @@ public class GameplayScreen extends AbstractScreen {
 
     @Override
     protected void init() {
-        initPlayer();
-        initPlayerButton();
-    }
+        Gdx.input.setInputProcessor(stage);
+       // initPlayer();
+      //  initPlayerButton();
 
-    private void initPlayerButton() {
-        playerButton = new Button(new Button.ButtonStyle());
-        playerButton.setWidth(460);
-        playerButton.setHeight(360);
-        playerButton.setX(10);
-        playerButton.setY(170);
-        playerButton.setDebug(true);
-        stage.addActor(playerButton);
 
-        playerButton.addListener(new ClickListener(){
+        final Skin skin = new Skin();
+        skin.add("default", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        skin.add("badlogic", new Texture("badlogic.jpg"));
 
-            @Override
-            public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
+        Image sourceImage = new Image(skin, "badlogic");
+        sourceImage.setBounds(50, 125, 100, 100);
+        stage.addActor(sourceImage);
 
-                player.reactOnClick();
+        Image validTargetImage = new Image(skin, "badlogic");
+        validTargetImage.setBounds(200, 50, 100, 100);
+        stage.addActor(validTargetImage);
 
-                System.out.println("CLICK");
-                return super.touchDown(event, x, y, pointer, button);
+        Image invalidTargetImage = new Image(skin, "badlogic");
+        invalidTargetImage.setBounds(200, 200, 100, 100);
+        stage.addActor(invalidTargetImage);
+
+        DragAndDrop dragAndDrop = new DragAndDrop();
+        dragAndDrop.addSource(new DragAndDrop.Source(sourceImage) {
+            public DragAndDrop.Payload dragStart (InputEvent event, float x, float y, int pointer) {
+                DragAndDrop.Payload payload = new DragAndDrop.Payload();
+                payload.setObject("Some payload!");
+
+                payload.setDragActor(new Label("Some payload!", skin));
+
+                Label validLabel = new Label("Some payload!", skin);
+                validLabel.setColor(0, 1, 0, 1);
+                payload.setValidDragActor(validLabel);
+
+                Label invalidLabel = new Label("Some payload!", skin);
+                invalidLabel.setColor(1, 0, 0, 1);
+                payload.setInvalidDragActor(invalidLabel);
+
+                return payload;
+            }
+        });
+        dragAndDrop.addTarget(new DragAndDrop.Target(validTargetImage) {
+            public boolean drag (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                getActor().setColor(Color.GREEN);
+                return true;
+            }
+
+            public void reset (DragAndDrop.Source source, DragAndDrop.Payload payload) {
+                getActor().setColor(Color.WHITE);
+            }
+
+            public void drop (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                System.out.println("Accepted: " + payload.getObject() + " " + x + ", " + y);
+            }
+        });
+        dragAndDrop.addTarget(new DragAndDrop.Target(invalidTargetImage) {
+            public boolean drag (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                getActor().setColor(Color.RED);
+                return false;
+            }
+
+            public void reset (DragAndDrop.Source source, DragAndDrop.Payload payload) {
+                getActor().setColor(Color.WHITE);
+            }
+
+            public void drop (DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
             }
         });
 
-    }
 
-    private void initPlayer() {
-        player = new Player();
-        stage.addActor(player);
     }
 
     @Override
